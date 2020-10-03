25e818ffdd59a883e326e8001348b60b3ecb5e1e
OnTheMend <andrii.maksakov@technorely.com>
2020-10-03 18:54:48 +0300

    Added new bar-chart

--- src/bar-chart.js
+++ src/bar-chart.js
@@ -1,17 +1,22 @@
 import React from 'react'
-import {View} from 'react-native'
-import {Svg, Rect, G} from 'react-native-svg'
+import { View } from 'react-native'
+import { Svg, Rect, G, Text, Line, LinearGradient, Stop } from 'react-native-svg'
 import AbstractChart from './abstract-chart'
 
-const barWidth = 32
+const barWidth = 10
 
 class BarChart extends AbstractChart {
+  getBarPercentage = () => {
+    const { barPercentage = 1 } = this.props.chartConfig;
+    return barPercentage;
+  };
+
   renderBars = config => {
-    const {data, width, height, paddingTop, paddingRight} = config
+    const { data, width, height, paddingTop, paddingRight } = config
     const baseHeight = this.calcBaseHeight(data, height)
     return data.map((x, i) => {
       const barHeight = this.calcHeight(x, data, height)
-      const barWidth = 32
+      const barWidth = 10
       return (
         <Rect
           key={Math.random()}
@@ -24,16 +29,17 @@ class BarChart extends AbstractChart {
             ((barHeight > 0 ? baseHeight - barHeight : baseHeight) / 4) * 3 +
             paddingTop
           }
+          rx={5}
           width={barWidth}
           height={(Math.abs(barHeight) / 4) * 3}
-          fill="url(#fillShadowGradient)"
+          fill="#FD7743"
         />
       )
     })
   }
 
   renderBarTops = config => {
-    const {data, width, height, paddingTop, paddingRight} = config
+    const { data, width, height, paddingTop, paddingRight } = config
     const baseHeight = this.calcBaseHeight(data, height)
     return data.map((x, i) => {
       const barHeight = this.calcHeight(x, data, height)
@@ -48,16 +54,216 @@ class BarChart extends AbstractChart {
           y={((baseHeight - barHeight) / 4) * 3 + paddingTop}
           width={barWidth}
           height={2}
-          fill={this.props.chartConfig.color(0.6)}
+          fill={'rgba(0,0,0,0)'}
         />
       )
     })
   }
 
+  renderValuesOnTopOfBars = ({
+    data,
+    width,
+    height,
+    paddingTop,
+    paddingRight
+  }: Pick<
+    Omit<AbstractChartConfig, "data">,
+    "width" | "height" | "paddingRight" | "paddingTop"
+  > & {
+    data: number[];
+  }) => {
+    const baseHeight = this.calcBaseHeight(data, height);
+
+    return data.map((x, i) => {
+      const barHeight = this.calcHeight(x, data, height);
+      const barWidth = 32 * this.getBarPercentage();
+
+      return (
+        <Text
+          key={Math.random()}
+          x={
+            paddingRight +
+            (i * (width - paddingRight)) / data.length +
+            barWidth / 1
+          }
+          y={((baseHeight - barHeight) / 4) * 3 + paddingTop - 7.5}
+          fill={'#23335B'}
+          fontSize="12"
+          textAnchor="middle"
+        >
+          {data[i]}
+        </Text>
+      );
+    });
+  };
+
+  renderHorLabels = config => {
+    const {
+      count,
+      data,
+      height,
+      paddingTop,
+      paddingRight,
+      yLabelsOffset = 12
+    } = config
+    const decimalPlaces = this.props.chartConfig.decimalPlaces === undefined ? 2 : this.props.chartConfig.decimalPlaces
+    const yAxisLabel = this.props.yAxisLabel || ''
+
+    return [...new Array(count)].map((_, i) => {
+      let yLabel
+
+      if (count === 1) {
+        yLabel = `${yAxisLabel}${data[0].toFixed(decimalPlaces)}`
+      } else {
+        const label = this.props.fromZero ?
+          (this.calcScaler(data) / (count - 1)) * i + Math.min(...data, 0) :
+          (this.calcScaler(data) / (count - 1)) * i + Math.min(...data)
+        yLabel = `${yAxisLabel}${label.toFixed(decimalPlaces)}`
+      }
+
+      return (
+        <Text
+          key={Math.random()}
+          x={paddingRight - yLabelsOffset}
+          textAnchor="end"
+          y={(height * 3) / 4 - ((height - paddingTop) / count) * i + 12}
+          fontSize={12}
+          fontWeight="700"
+          fill={'#9BA1AE'}
+        >
+          {parseInt(yLabel) !== 0 && parseInt(yLabel) + 'k'}
+        </Text>
+      )
+    })
+  }
+
+  renderVerLabels = config => {
+    const {
+      labels = [],
+      width,
+      height,
+      paddingRight,
+      paddingTop,
+      horizontalOffset = 0,
+      stackedBar = false
+    } = config
+    const fontSize = 12
+    let fac = 1
+    if (stackedBar) {
+      fac = 0.71
+    }
+
+    return labels.map((label, i) => {
+      return (
+        <Text
+          key={Math.random()}
+          x={
+            (((width - paddingRight) / labels.length) * i +
+              paddingRight +
+              horizontalOffset) *
+            fac
+          }
+          y={(height * 3) / 4 + paddingTop + fontSize * 2}
+          fontSize={12}
+          fill={'#9BA1AE'}
+          textAnchor="middle"
+          fontWeight="700"
+        >
+          {label}
+        </Text>
+      )
+    })
+  }
+
+  renderHorLines = config => {
+    const { width, height, paddingTop, paddingRight } = config
+    return (
+      <G>
+        <Line
+          key={Math.random()}
+          x1={paddingRight}
+          y1={height - height / 4 + paddingTop - paddingTop * 2.25}
+          x2={width}
+          y2={height - height / 4 + paddingTop - paddingTop * 2.25}
+          stroke={'rgba(240, 240, 248, 1)'}
+          strokeDasharray="0, 0"
+          fill={'rgba(240, 240, 248, 1)'}
+          strokeWidth={1}
+        />
+        <Line
+          key={Math.random()}
+          x1={paddingRight}
+          y1={height - height / 4 + paddingTop - paddingTop * 4.5}
+          x2={width}
+          y2={height - height / 4 + paddingTop - paddingTop * 4.5}
+          stroke={'rgba(240, 240, 248, 1)'}
+          strokeDasharray="0, 0"
+          fill={'rgba(240, 240, 248, 1)'}
+          strokeWidth={1}
+        />
+        <Line
+          key={Math.random()}
+          x1={paddingRight}
+          y1={height - height / 4 + paddingTop - paddingTop * 6.75}
+          x2={width}
+          y2={height - height / 4 + paddingTop - paddingTop * 6.75}
+          stroke={'rgba(240, 240, 248, 1)'}
+          strokeDasharray="0, 0"
+          fill={'rgba(240, 240, 248, 1)'}
+          strokeWidth={1}
+        />
+         <Line
+          key={Math.random()}
+          x1={paddingRight}
+          y1={height - height / 4 + paddingTop - paddingTop * 9}
+          x2={width}
+          y2={height - height / 4 + paddingTop - paddingTop * 9}
+          stroke={'rgba(240, 240, 248, 1)'}
+          strokeDasharray="0, 0"
+          fill={'rgba(240, 240, 248, 1)'}
+          strokeWidth={1}
+        />
+         <Line
+          key={Math.random()}
+          x1={paddingRight}
+          y1={height - height / 4 + paddingTop - paddingTop * 11.25}
+          x2={width}
+          y2={height - height / 4 + paddingTop - paddingTop * 11.25}
+          stroke={'rgba(240, 240, 248, 1)'}
+          strokeDasharray="0, 0"
+          fill={'rgba(240, 240, 248, 1)'}
+          strokeWidth={1}
+        />
+         <Line
+          key={Math.random()}
+          x1={paddingRight}
+          y1={height - height / 4 + paddingTop - paddingTop * 13.5}
+          x2={width}
+          y2={height - height / 4 + paddingTop - paddingTop * 13.5}
+          stroke={'rgba(240, 240, 248, 1)'}
+          strokeDasharray="0, 0"
+          fill={'rgba(240, 240, 248, 1)'}
+          strokeWidth={1}
+        />
+        <Line
+          key={Math.random()}
+          x1={paddingRight}
+          y1={height - height / 4 + paddingTop}
+          x2={width}
+          y2={height - height / 4 + paddingTop}
+          stroke={'rgba(240, 240, 248, 1)'}
+          strokeDasharray="0, 0"
+          fill={'rgba(240, 240, 248, 1)'}
+          strokeWidth={1}
+        />
+      </G>
+    )
+  }
+
   render() {
     const paddingTop = 16
-    const paddingRight = 64
-    const { 
+    const paddingRight = 62.5
+    const {
       width,
       height,
       data,
@@ -65,7 +271,7 @@ class BarChart extends AbstractChart {
       withHorizontalLabels = true,
       withVerticalLabels = true,
     } = this.props
-    const {borderRadius = 0} = style
+    const { borderRadius = 0 } = style
     const config = {
       width,
       height
@@ -82,10 +288,10 @@ class BarChart extends AbstractChart {
             height={height}
             rx={borderRadius}
             ry={borderRadius}
-            fill="url(#backgroundGradient)"
+            fill="rgba(0,0,0,0)"
           />
           <G>
-            {this.renderHorizontalLines({
+            {this.renderHorLines({
               ...config,
               count: 4,
               paddingTop
@@ -93,25 +299,25 @@ class BarChart extends AbstractChart {
           </G>
           <G>
             {withHorizontalLabels
-              ? this.renderHorizontalLabels({
-              ...config,
-              count: 4,
-              data: data.datasets[0].data,
-              paddingTop,
-              paddingRight
-            })
-            : null}
+              ? this.renderHorLabels({
+                ...config,
+                count: 4,
+                data: data.datasets[0].data,
+                paddingTop,
+                paddingRight
+              })
+              : null}
           </G>
           <G>
             {withVerticalLabels
-              ? this.renderVerticalLabels({
-              ...config,
-              labels: data.labels,
-              paddingRight,
-              paddingTop,
-              horizontalOffset: barWidth
-            })
-            : null}
+              ? this.renderVerLabels({
+                ...config,
+                labels: data.labels,
+                paddingRight,
+                paddingTop,
+                horizontalOffset: barWidth
+              })
+              : null}
           </G>
           <G>
             {this.renderBars({
@@ -122,13 +328,23 @@ class BarChart extends AbstractChart {
             })}
           </G>
           <G>
+            {this.renderValuesOnTopOfBars({
+              ...config,
+              data: data.datasets[0].data,
+              paddingTop: paddingTop,
+              paddingRight: paddingRight
+            })}
+          </G>
+
+          {/* NO NEED HERE */}
+          {/* <G>
             {this.renderBarTops({
               ...config,
               data: data.datasets[0].data,
               paddingTop,
               paddingRight
             })}
-          </G>
+          </G> */}
         </Svg>
       </View>
     )
